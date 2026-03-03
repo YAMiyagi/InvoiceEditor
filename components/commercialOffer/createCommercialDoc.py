323bcbad3d93a3b2a762737d3d1e96ad9544871f
YAMiyagi <tenisbek.miyagi@gmail.com>
2026-03-04 00:34:09 +0600

    исправлен перенос текста в кп

--- components/commercialOffer/createCommercialDoc.py
+++ components/commercialOffer/createCommercialDoc.py
@@ -1,6 +1,5 @@
 from components.num2words.num2words import convertNum2Words
 from components.productTables.productTables import add_product_tables
-from reportlab.pdfbase import pdfmetrics
 kp_title = [
     'Алапаев Адилет',
     'Умуш Аскар'
@@ -23,9 +22,11 @@ def createCommercialDoc(doc, data, signPath, propsIndex, loadJson, date, months,
         text=f"Исходный документ №{data['docNum']} от {date['currDay']} {months[date['currMonth'] - 1]} {date['currYear']}г.",
         font_size=10
     )
-    doc.add_text(x=185, y=620, text="Коммерческое предложение", font="Arial-Thick", font_size = 15)
-    doc.add_text(x=120, y=580, text_area_width = 400, text=f'Уважаемый {data["clientName"].get()}, Благодарим за интерес к нашим услугам.', font_size=11)
-    doc.add_text(x=90, y=560, text_area_width = 400, text=f'Мы предлагаем рассмотреть наше предложение по реализации {data["offer"].get()}:', font_size=11)
+    doc.add_text(x=175, y=620, text="Коммерческое предложение", font="Arial-Thick", font_size = 15)
+    doc.add_text(x=100, y=580,
+                    text_area_width = 400,
+                    text=f'Уважаемый {data["clientName"].get()}, Благодарим за интерес к нашим услугам. Мы предлагаем рассмотреть наше предложение по реализации {data["offer"].get()}:', 
+                    font_size=11)
     doc.add_image(path="data\img\emin.png", x=70, y=720, width=140, height=55)
     doc.add_stroke(x=50, y=690, x2=700, y2=690, weight=3)
     
--- pdf/generator.py
+++ pdf/generator.py
@@ -1,5 +1,3 @@
-import math
-
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.colors import black
